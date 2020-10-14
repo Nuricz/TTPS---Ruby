bad4e37c937d10642b7f153a0898d2210e34751f
nuricz <nuria.czajko@gmail.com>
2020-10-14 12:42:44 -0300

    ActiveRecord - punto 6

---
+++ práctica-5/practica_cinco/app/models/office.rb
@@ -0,0 +1,8 @@
+class Office < ApplicationRecord
+    def to_s
+        "Nombre: " + name.to_s + "\n" +
+        "Teléfono: " + phone_number.to_s + "\n" +
+        "Dirección: " + address.to_s + "\n" +
+        "Disponible: " + available.to_s + "\n"
+    end
+end
