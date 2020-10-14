d0f49ea6cd37c64c864a4fedac6d4ed18f059056
nuricz <nuria.czajko@gmail.com>
2020-10-14 12:41:59 -0300

    Corrección ortográfica

--- práctica-5/practica_cinco/db/migrate/20201014153139_create_office.rb
+++ práctica-5/practica_cinco/db/migrate/20201014153139_create_office.rb
@@ -3,7 +3,7 @@ class CreateOffice < ActiveRecord::Migration[6.0]
     create_table :offices do |t|
       t.string :name, limit: 255, null: false
       t.string :phone_number, limit: 30, null: false
-      t.text :adress
+      t.text :address
       t.boolean :available, null: false, default: false
     end
   end
