028ad4094252ae382f2951ec53b309d32c51f83e
nuricz <nuria.czajko@gmail.com>
2020-11-09 11:59:22 -0300

    Add routes to employees y offices

--- práctica-5/practica_cinco/config/routes.rb
+++ práctica-5/practica_cinco/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :employees, :except => :delete
+  resources :offices, :except => :delete
   get '/', action: :salute, controller: 'polite'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
